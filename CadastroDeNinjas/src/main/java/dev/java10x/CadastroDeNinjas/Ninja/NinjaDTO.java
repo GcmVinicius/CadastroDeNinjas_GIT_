0dd00b780964e8b6f5b051c7e7b2a2cabdcdafb3
Vinicius Machado <viniciusgabrielcoutomachado@gmail.com>
2025-12-08 12:35:27 -0300

    fix: tira a responsabilidade do model e passa para o DTO

--- CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Ninja/NinjaDTO.java
+++ CadastroDeNinjas/src/main/java/dev/java10x/CadastroDeNinjas/Ninja/NinjaDTO.java
@@ -1,4 +1,29 @@
 package dev.java10x.CadastroDeNinjas.Ninja;
 
+import dev.java10x.CadastroDeNinjas.Missoes.MissoesModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+
+    //o id logo abaixo será detalhado como id da entidade e o generated gera uma estratégia para o id
+
+    private Long id;
+
+    private String nome;
+
+    private String email;
+
+    private String imgUrl;
+
+    private int idade;
+
+    private MissoesModel missoes;
+
+    private String rank;
 }
